bdbac31ad0b6860abcd4d3628c4e3401c9039bc1
thaihoc111202 <coderthichhat@gmail.com>
2026-04-17 01:57:59 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,6 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const db = mysql.createPool(process.env.DATABASE_URL);
 
 // Test DB
 db.getConnection((err, connection) => {
